9829f355a464f2358c2a472c4a05ede14f0ae187
Sâmyla <samylamaria14@gmail.com>
2022-06-10 14:49:47 -0300

    formulario

---
+++ crud-pessoa1/src/main/java/com/ifce/br/controller/PessoaController.java
@@ -0,0 +1,18 @@
+package com.ifce.br.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PessoaController {
+	@GetMapping("/olaMundo")
+	public String olaMundo()
+{
+		return "ola-mundo";
+	}
+	@GetMapping("pessoa/formulario")
+	public String formulario() {
+		return "formulario";
+	}
+
+}
